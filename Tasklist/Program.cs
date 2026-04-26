8f3e932bbf3e443c7b927fbe9b4950468dc5ffb7
dobbe <dobbe@COMPUTERSEPPE>
2026-04-26 12:28:24 +0200

    Add project files.

---
+++ Tasklist/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] taskNames = new string[100];
+        string[] deadlines = new string[100];
+        bool[] isDone = new bool[100];
+        string[] doneDates = new string[100];
+        int taskCount = 0;
+    }
+}
